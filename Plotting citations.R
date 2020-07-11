d73e5028e5a92d18c8e23ac63c0245bda684da24
PhilipDW183 <55950812+PhilipDW183@users.noreply.github.com>
2020-07-11 09:25:00 +0100

    Comments to R added

--- Plotting citations.R
+++ Plotting citations.R
@@ -19,26 +19,22 @@ library(sf)
 library(sp)
 library(raster)
 
+#make sure that the working directory is set for easy use
 setwd("C:/Users/cex/Documents/Smart Cities and Urban Analytics/Dissertation/Data")
 
-tl2 <- readOGR("shapefiles/OECD_TL2_2020.shp")
-#plot(tl2)
+#The same files as in the previous workbook are read in as necessary for plotting
 
 tl3 <- readOGR("shapefiles/OECD_TL3_2020.shp")
-#plot(tl3)
 
 US_counties <- readOGR("shapefiles/cb_2013_us_county_5m.shp")
-#plot(US_counties)
 
 Europe_2013 <- readOGR("shapefiles/NUTS_RG_01M_2013_4326_LEVL_3.shp/NUTS_RG_01M_2013_4326_LEVL_3.shp")
 plot(Europe_2013)
 
 Europe_2010 <- readOGR("shapefiles/NUTS_RG_01M_2010_4326_LEVL_3.shp/NUTS_RG_01M_2010_4326_LEVL_3.shp")
-
+#extract the UK from 2010 and 2013
 UK_2010 = subset(Europe_2010, CNTR_CODE=="UK")
 Europe_2013_noUK = subset(Europe_2013, CNTR_CODE !="UK")
-
-TL2_ISR = subset(tl2, iso3=="ISR")
 
 #non OECD countries
 China <- readOGR("shapefiles/Non-OECD/gadm36_CHN_1.shp")
@@ -51,37 +47,38 @@ Russia <- readOGR("shapefiles/Non-OECD/gadm36_RUS_1.shp")
 
 South_Africa <- readOGR("shapefiles/Non-OECD/gadm36_ZAF_1.shp")
 
+#World
 world <- readOGR("shapefiles/99bfd9e7-bb42-4728-87b5-07f8c8ac631c2020328-1-1vef4ev.lu5nk.shp")
 
+#Other countries
 HK <- readOGR("shapefiles/gadm36_HKG_0.shp")
 
 Monaco <- readOGR("shapefiles/gadm36_MCO_0.shp")
 
+#Isle of mann to be merged with GB
 IOM <- readOGR("shapefiles/gadm36_IMN_0.shp")
-
+#set the NUTS_ID
 IOM$NUTS_ID <- "IM000"
+#change the name of GID_0 
 names(IOM)[names(IOM) == "GID_0"] <- "CNTR_CODE"
+#Change the country name
 IOM$CNTR_CODE <- "GB"
+#Change the NUTS_Name
 IOM$NUTS_NAME <- "Isle of Man"
-
+#Drop unecessary columns from GB
 UK_drops <- c("LEVL_CODE", "FID")
 UK_2010 <- UK_2010[,!(names(UK_2010) %in% UK_drops)]
-
+#Drop unecessary columns from IOM
 IOM_drops <- c("NAME_0")
 IOM <- IOM[,!(names(IOM) %in% IOM_drops)]
-
-
+#Add the IoM to the UK
 UK <- rbind(UK_2010, IOM)
 
 
 #read in patent data
 UK_patents <- read_csv("Patents data/all_UK_patents_region.csv")
 UK_cited <- read_csv("Patents data/cited_regions_count.csv")
-
-sum(UK_patents$inv_share)
-
-sum(UK_cited$inv_share)
-
+#Merge these with the UK
 UK <- merge(UK,
             UK_patents,
             by.x = "NUTS_ID", 
@@ -91,34 +88,44 @@ UK  <- merge(UK,
              by.x = "NUTS_ID",
              by.y = "reg_code")
 
-
-display.brewer.all()
-
+#set tmap mode to plot
 tmap_mode("plot")
+#All of the subsequent plots are exported using the export button
 
+#The first map is all patents in the UK
 tm1 <- tm_shape(UK)+
+      #fill with inventor share
       tm_fill("inv_share.x",
+              #set alpha to 1 so no transparency
               alpha =1,
+              #set the title
               title = "UK regional inventor share",
+              #set the palette - single colour
               palette = "Oranges",
+              #set the legend to show
               legend.show = TRUE)+
+      #add borders to the map
       tm_borders(col = "grey",
                  alpha = 0.8)+
+      #set conditions for the legend
       tm_legend(title.size =1.5,
                 show = TRUE)+
+  #Use layout to set the title, and legend conditions
   tm_layout(title = "UK originating patents",
             title.size = 2,
             title.position = c("left","top"),
             legend.height = 4,
             legend.width = 4,
             legend.text.size = 1.2)+
+  #add a compass to the top right corned
   tm_compass(type="rose",
              position = c(0.75,0.8),
              color.light ="grey90",
              size = 4)
-
+#output the result
 tm1
 
+#PRoduce the same output but for patents that were cited in the UK
 tm_2 <- tm_shape(UK)+
   tm_fill("inv_share.y",
           alpha =1,
@@ -144,19 +151,18 @@ tm_2
 
 #all citing patents location
 citing <- read_csv("Patents data/citing_regions_count.csv")
-
+#create a histogram shwoing the distribution
 hist(citing$inv_share,
      breaks = 1000)
-
+#set the breaks according to this histogram
 breaks = c(0,25,50,100,250,500,1000,10000)
-
-tmap_mode("view")
-
+#Add to the UK map
 UK <- merge(UK,
             citing,
             by.x = "NUTS_ID",
             by.y = "reg_code")
 
+#All maps should have the same legend so the first map is legend only
 tm_shape(UK)+
   tm_fill("inv_share",
           alpha =1,
@@ -174,12 +180,14 @@ tm_shape(UK)+
             legend.height = 4,
             legend.width = 4,
             legend.text.size = 1.2,
+            #This means only the legend is outputted
             legend.only =  TRUE)
 
-
+#The first map is the UK
 tm_shape(UK)+
   tm_fill("inv_share",
           alpha =1,
+          #set the title to a) to be referenced in the caption
           title = "a)",
           breaks = breaks,
           legend.show = FALSE,
@@ -200,26 +208,18 @@ tm_shape(UK)+
              size = 4)
 
 
- # tm_compass(type="rose",
- #            position = c(0.75,0.8),
- #           color.light ="grey90",
- #            size = 4)
-
+#The next map is the US
 US <- merge(US_counties,
             citing,
             by.x = "GEOID",
             by.y = "reg_code")
 
-tmap_mode("view")
-
+#counties can be removed that are not necessary to show the US
 remove <- c("02185", "02188","02290","02180","02090","02240","02068","02270","02050","02170","02261","02070","02164","02060","02122","02020","02261","02282","02105","02100","02230","02110","02105","02220","02195","02275","02130","02198","02275","02150","02164","02013","02016",15001,15009,15003,15007,60010,60050,60020,60400,60030,72097,72117,72011,72003,72097,72067,72023,72079,72125,72093,72083,72131,72099,72005,72071,72115,72027,72081,72153,72055,72121,72059,72111,72001,72141,72065,72013,72017,72054,72039,72073,72113,72075,72419,72107,72039,72054,72017,72091,72145,72101,72107,72149,72075,72133,72043,72107,72101,72145,72143,72051,72137,72135,72105,72019,72045,72009,72043,72133,72123,72057,72035,72057,72015,72041,72007,72021,72061,72127,72139,72063,72129,72109,72151,72095,72085,72077,72063,72139,72127,72031,72029,72087,72119,72103,72069,72037,72052,72089,72147,72049,78030,78020,78010,66010,69100,69120,69110,69085,02016, 15005,72047,72025,72033,72053,"02016",60040)
-
 US <- US[! US$GEOID %in% remove,]
-
+#plot the result to make sure it is correct
 plot(US)
-
-tmap_mode("plot")
-
+#plot the result with the same format as before
 tm3 <- tm_shape(US)+
   tm_fill("inv_share",
           alpha =1,
@@ -245,22 +245,18 @@ tm3 <- tm_shape(US)+
 
 tm3
 
+#Merge Europe with citing
 Europe <- merge(Europe_2013_noUK,
                 citing,
                 by.x = "NUTS_ID",
                 by.y = "reg_code")
-
-
-tmap_mode("view")
-
+#remove regions that obstruct the view
 remove_EU <- c("PT200","PT300","ES704", "ES706","ES705","ES709","ES708","ES706",
                "ES703","ES707","FRA50","FRA40","IS002","IS001","FRA30","FRA20",
                "FRA10")
-
 Europe <- Europe[! Europe$NUTS_ID %in% remove_EU,]
 
-tmap_mode("plot")
-
+#plot the result
 tm_shape(Europe)+
   tm_fill("inv_share",
           alpha =1,
@@ -288,6 +284,7 @@ citing$County <- substr(citing$reg_code, start =1, stop =2)
 
 count <- aggregate(citing$inv_share, by = list(Category = citing$County), FUN=sum)
 
+#For China, regional codes need to be changed to match those from the OECD REGPAT database
 China$GID_1 <- substr(China$GID_1, start = 1, stop = 6)
 China$GID_1 <- gsub("_", "", China$GID_1)
 China$GID_1 <- gsub("\\.", "", China$GID_1)
@@ -325,28 +322,30 @@ China$GID_1 <- sub("CHN21", "CN29", China$GID_1)
 #China$GID_1 <- gsub("CHN", "CN", China$GID_1)
 #China$GID_1 <- gsub("CN19", "CN191", China$GID_1)
 
+#We also want to extract JAP and KOR
 JPN = subset(tl3, iso3=="JPN")
 KOR = subset(tl3, iso3=="KOR")
-
+#bind these together
 JP_KR <- rbind(JPN, KOR)
-
+#Drop unecessary rows
 TL3_drops = c("tl2_id", "tl1_id", "name_or", "name_en", "name_fr", "continenta", "reg_name_p")
+#Remove these from the shapefile
 JP_KR <- JP_KR[,!(names(JP_KR) %in% TL3_drops)]
-
+#drop unecessary rows for china
 China_drops = c("NAME_0", "NAME_1", "VARNAME_1", "NL_NAME_1", "TYPE_1", "ENGTYPE_1", "CC_1", "HASC_1")
 China <- China[,!(names(China) %in% China_drops)]
-
+#set the codes to match
 names(China)[names(China) == "GID_0"] <- "iso3"
 names(China)[names(China) == "GID_1"] <- "reg_code"
 names(JP_KR)[names(JP_KR) == "tl3_id"] <- "reg_code"
-
+#merge them all together
 JP_KR_CN <- rbind(JP_KR, China)
-
+#merge these with citing regions
 JP_KR_CN <- merge(JP_KR_CN,
                   citing,
                   by.x = "reg_code",
                   by.y= "reg_code")
-
+#plot the output as result
 tm_shape(JP_KR_CN)+
   tm_fill("inv_share",
           alpha =1,
--- merging_location.R
+++ merging_location.R
@@ -18,12 +18,15 @@ library(sf)
 library(sp)
 library(raster)
 
+#The working directory is checked
 getwd()
 
+#the working directory is set to one that makes it easier to collect outputs and inputs
 setwd("C:/Users/cex/Documents/Smart Cities and Urban Analytics/Dissertation/Data")
 
-#https://stackoverflow.com/questions/38574695/sql-server-2008-merge-statement-multiple-match-conditions
 
+#The first attempt was to extract city names from the patent data and use OSM, Google maps or SQL merges to merge the results based on city level data
+#https://stackoverflow.com/questions/38574695/sql-server-2008-merge-statement-multiple-match-conditions
 #https://stackoverflow.com/questions/54637189/merging-two-tables-where-one-column-is-substring-of-the-other-in-r
 
 data <- read_csv("Patents data/EPO_citing_for_OSM.csv")
@@ -98,72 +101,94 @@ EPO_ind_ctrl_PCT_merged = EPO_ind_ctrl_PCT_merged[-c(30,29,28,27,26,25,24,23,22,
 #write.csv(EPO_ind_ctrl_PCT_merged, "Patents data/EPO_ind_ctrl_PCT_with_city.csv")
 
 
-#found from here: https://gis.stackexchange.com/questions/147538/where-can-i-find-shapefiles-for-oecd-regions
+#The second (more successful) attempt used the regions specified in the OECD REGPAT database to extract centroids and hence use these to calculate the distance between patents
+
+#The OECD regions were found from:
+#https://gis.stackexchange.com/questions/147538/where-can-i-find-shapefiles-for-oecd-regions
 #https://www.dropbox.com/sh/aqfzuofxocv6zgl/AABFWmuLByljQrvY1eXP8em8a?dl=0 FOR TL2 and TL3 shapefiles
-#need to find NUTS3 shapefiles for 2013
-#https://gis.stackexchange.com/questions/282231/any-way-to-extract-nuts-3-shapefile-of-germany-from-eurostat-shapefile-in-r-up
 
+#tl2 specification for regions
 tl2 <- readOGR("shapefiles/OECD_TL2_2020.shp")
-#plot(tl2)
+plot(tl2)
 
+#tl3 specification for regions
 tl3 <- readOGR("shapefiles/OECD_TL3_2020.shp")
 plot(tl3)
 
+#US counties were extracted from:
+#https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html
 US_counties <- readOGR("shapefiles/cb_2013_us_county_5m.shp")
+#Plot The result to make sure it is correct
 plot(US_counties)
+#extract the centroids from the US counties
 US_2013_centroids =gCentroids(US_counties, byid= TRUE)
 
+#European NUTS boundaries were extracted from:
+#https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/nuts
 Europe_2013 <- readOGR("shapefiles/NUTS_RG_01M_2013_4326_LEVL_3.shp/NUTS_RG_01M_2013_4326_LEVL_3.shp")
+#plot the shapefile to check its shape
 plot(Europe_2013)
+#Extract the EU centroids
 EU_2013_centroids = gCentroid(Europe_2013, byid = TRUE)
+#plot the results
 plot(EU_centroids)
+#However we don't want the UK so subset where CNTR_code is not UK
+Europe_2013_noUK = subset(Europe_2013, CNTR_CODE !="UK")
+#plot the results
+plot(Europe_2013_noUK)
 
 Europe_2010 <- readOGR("shapefiles/NUTS_RG_01M_2010_4326_LEVL_3.shp/NUTS_RG_01M_2010_4326_LEVL_3.shp")
-EU_2010_centroid = gCentroid(Europe_2010, byid = TRUE)
-
-#https://gis.stackexchange.com/questions/43543/how-to-calculate-polygon-centroids-in-r-for-non-contiguous-shapes/43558
-
+#For Europe 2010 only the UK is needed
+#Thus subset on:
 UK_2010 = subset(Europe_2010, CNTR_CODE=="UK")
-Europe_2013_noUK = subset(Europe_2013, CNTR_CODE !="UK")
+#plot the UK to make sure it is correct
 plot(UK_2010)
-plot(Europe_2013_noUK)
+#Then the centroids for the UK can be extracted at the NUTS3 level
 UK_centroids = gCentroid(UK_2010, byid = TRUE, id= NUTS_ID)
-plot(UK_centroids)
+
+#The issue with these however are the several columns that are retained that we don't want and/or need
 
 #UK_2010_SF <- st_as_sf(UK_2010)
 #UK_2010_centroids <- st_centroid(UK_2010_SF)
 #join <- join(UK_2010_SF, UK_2010_centr#oids)
 
+#The first attempt was to extract only the NUTS_ID when getting centroids
 UK_2010_centroids <- SpatialPointsDataFrame(gCentroid(UK_2010, byid=TRUE),UK_2010@data["NUTS_ID"],  match.ID=FALSE)
 
+#The second attempt used all the data when getting centroids
 UK_2010_centroids2 <- SpatialPointsDataFrame(gCentroid(UK_2010, byid=TRUE),UK_2010@data,  match.ID=FALSE)
-
+#Then create a list of columns that we wanted to drop
 UK_drops <- c("LEVL_CODE", "NUTS_NAME", "FID")
+#Then remove those columns from the resulting dataframe
 UK_2010_centroids2 <- UK_2010_centroids2[,!(names(UK_2010_centroids2) %in% UK_drops)]
 
+#The same process was then repeated for the European centroids datframe
 EUROPE_2013_centroids <- SpatialPointsDataFrame(gCentroid(Europe_2013_noUK, byid=TRUE),Europe_2013_noUK@data["NUTS_ID"], match.ID=FALSE)
-
 EUROPE_2013_centroids2 <- SpatialPointsDataFrame(gCentroid(Europe_2013_noUK, byid=TRUE),Europe_2013_noUK@data, match.ID=FALSE)
 EU_drops <- c("LEVL_CODE", "NUTS_NAME", "FID")
 EUROPE_2013_centroids2 <- EUROPE_2013_centroids2[,!(names(EUROPE_2013_centroids2) %in% EU_drops)]
 
+#For the US, GEOID was used as the regional code
 US_2013_centroids <- SpatialPointsDataFrame(gCentroid(US_counties, byid=TRUE),US_counties@data["GEOID"], match.ID=FALSE)
+#The country code could be assigned as US since it is all one country
 US_2013_centroids$CNTR_CODE <- "US"
-
+#The second version used all the data
 US_2013_centroids2 <- SpatialPointsDataFrame(gCentroid(US_counties, byid=TRUE),US_counties@data, match.ID=FALSE)
-#US <- list(rep("US", 3234))
 
+#The same process was used for TL3 centroids
 TL3_centroids <- SpatialPointsDataFrame(gCentroid(tl3, byid=TRUE),tl3@data["tl3_id"], match.ID=FALSE)
 
 TL3_centroids2 <- SpatialPointsDataFrame(gCentroid(tl3, byid=TRUE),tl3@data, match.ID=FALSE)
 TL3_drops = c("tl2_id", "tl1_id", "name_or", "name_en", "name_fr", "continenta", "reg_name_p")
 TL3_centroids2 <- TL3_centroids2[,!(names(TL3_centroids2) %in% TL3_drops)]
+#Only countries who were in the OECD REGPAT classification as occuring under TL3 regional classification were required
 TL3_countries <- c("AUS", "CAN", "CHL", "JPN", "KOR", "MEX", "NZL", "CHN", "IND", "RUS", "SAU", "ZAF")
+#Thus we get a subset of the countries (although some countries were missing)
 TL3_centroids_small <- TL3_centroids2[TL3_centroids2$iso3 %in% TL3_countries,]
 
-
-
+#For TL2 specification, only ISR was requried 
 TL2_ISR = subset(tl2, iso3=="ISR")
+#The same process was repeated
 TL2_centroids <- SpatialPointsDataFrame(gCentroid(tl2, byid=TRUE),tl2@data["tl2_id"], match.ID=FALSE)
 
 TL2_centroids2 <- SpatialPointsDataFrame(gCentroid(TL2_ISR, byid=TRUE),TL2_ISR@data, match.ID=FALSE)
@@ -171,30 +196,34 @@ TL2_drops <- c("tl1_id", "name_or", "name_en", "name_fr", "continenta", "reg_nam
 TL2_centroids2 <- TL2_centroids2[,!(names(TL2_centroids2) %in% TL2_drops)]
 plot(TL2_centroids2)
 
-
+#The next stage was to bind all of these together into a single dataframe
 centroids2 <- rbind(UK_2010_centroids2, EUROPE_2013_centroids2)
 
+#This required the changing of names of columns so that they could be vertically stacked
 names(TL3_centroids_small)[names(TL3_centroids_small) == "tl3_id"] <- "region_code"
 names(TL3_centroids_small)[names(TL3_centroids_small) == "iso3"] <- "CNTR_CODE"
 names(TL2_centroids2)[names(TL2_centroids2) == "tl2_id"] <- "region_code"
 names(TL2_centroids2)[names(TL2_centroids2) == "iso3"] <- "CNTR_CODE"
-
+#Thus another merged dataframe could be identified
 centroids_TL <- rbind(TL3_centroids_small, TL2_centroids2)
 
+#again, the name was changed in the centroids 2 dataframe
 names(centroids2)[names(centroids2) == "NUTS_ID"] <- "region_code"
-
+#thus, this could be bound together 
 centroids2 <- rbind(centroids2, centroids_TL)
-
+#The next thing is to make sure they have the same CRS
 centroids2 <- spTransform(centroids2, CRS("+init=epsg:4326"))
+#US names have to be changed
 names(US_2013_centroids)[names(US_2013_centroids) == "GEOID"] <- "region_code"
+#CRS has to make sre it is correct
 US_2013_centroids <- spTransform(US_2013_centroids, CRS("+init=epsg:4326"))
-
+#Then all can be bound together
 centroids2 = rbind(centroids2, US_2013_centroids)
-
+#And outputted to a shapefile
 writeOGR(centroids2, "shapefiles/centroids2.shp", layer ="centroids", driver="ESRI Shapefile")
 
 
-#old
+#This is a previous solution
 centroids <- rbind(UK_2010_centroids, EUROPE_2013_centroids)
 
 names(TL3_centroids)[names(TL3_centroids) == "tl3_id"] <- "region_code"
@@ -214,46 +243,76 @@ centroids = rbind(centroids, US_2013_centroids)
 writeOGR(centroids, "shapefiles/centroids_test.shp", layer ="centroids", driver="ESRI Shapefile")
 
 
-#non OECD countries
+#The issue with the previous work is that several key countries are still missing
+#This includes non OECD countries that the OECD REGPAT classifies under the TL2 and TL3 conditions
+#Thus non OECD countries boundaries were extracted from:
+#https://gadm.org/
+
+#The same spatial points process for getting centroids was repeated
 China <- readOGR("shapefiles/Non-OECD/gadm36_CHN_1.shp")
+#plot the result to make sure it is correct
 plot(China)
+#extract centroids, but only retain GID_1 as the regional title
 China_centroids <- SpatialPointsDataFrame(gCentroid(China, byid=TRUE),China@data["GID_1"], match.ID=FALSE)
+#country code was assigned as CN - the 2 digit county code
 China_centroids$CNTR_CODE <- "CN"
 
 India <- readOGR("shapefiles/Non-OECD/gadm36_IND_1.shp")
 plot(India)
 India_centroids <- SpatialPointsDataFrame(gCentroid(India, byid=TRUE),India@data["GID_1"], match.ID=FALSE)
+#Country code assigned as IN
 India_centroids$CNTR_CODE <- "IN"
 
 Brazil <- readOGR("shapefiles/Non-OECD/gadm36_BRA_1.shp")
 plot(Brazil)
 Brazil_centroids <- SpatialPointsDataFrame(gCentroid(Brazil, byid=TRUE),Brazil@data["GID_1"], match.ID=FALSE)
+#country code set as BR
 Brazil_centroids$CNTR_CODE <- "BR"
 
 Russia <- readOGR("shapefiles/Non-OECD/gadm36_RUS_1.shp")
 plot(Russia)
 Russia_centroids <- SpatialPointsDataFrame(gCentroid(Russia, byid=TRUE),Russia@data["GID_1"], match.ID=FALSE)
+#county code set as RU
 Russia_centroids$CNTR_CODE <- "RU"
 
 South_Africa <- readOGR("shapefiles/Non-OECD/gadm36_ZAF_1.shp")
 plot(South_Africa)
 ZA_centroids <- SpatialPointsDataFrame(gCentroid(South_Africa, byid=TRUE),South_Africa@data["GID_1"], match.ID=FALSE)
+#Country code set as ZA
 ZA_centroids$CNTR_CODE <- "ZA"
 
-
+#All of these have the same CRS and so can easily be merged together to form a single dataframe
 Non_OECD_Cent <- rbind(ZA_centroids, Russia_centroids)
 Non_OECD_Cent2 <- rbind(Brazil_centroids, China_centroids)
 Non_OECD_Cent <- rbind(Non_OECD_Cent, Non_OECD_Cent2)
 Non_OECD_Cent <- rbind(Non_OECD_Cent, India_centroids)
+#Set the same CRS as those in the conditions above
 Non_OECD_Cent <- spTransform(Non_OECD_Cent, CRS("+init=epsg:4326"))
-
+#Change the title of the regional column to region_code (the same as the previous specification)
 names(Non_OECD_Cent)[names(Non_OECD_Cent) == "GID_1"] <- "region_code"
 
-
+#Write this out as a shapefile
 writeOGR(Non_OECD_Cent, "shapefiles/Non_OECD_cent.shp", layer ="centroids", driver="ESRI Shapefile")
 
+#Still, world countries are missing, where regions are assigned as the centre of the country
+#This also comes from the same source as above
 world <- readOGR("shapefiles/99bfd9e7-bb42-4728-87b5-07f8c8ac631c2020328-1-1vef4ev.lu5nk.shp")
+#The 2 digit county codes are then read in
+countries <- read_csv("country_codes.csv")
+#This is then merged so that county name is replaced by the 2 digit county code
+world <- merge(world,
+               countries,
+               by.x = "CNTRY_NAME",
+               by.y = "Name")
+#centroids are then extracted, retaining only the 2 digit code
+world_centroids <- SpatialPointsDataFrame(gCentroid(world, byid=TRUE),world@data["Code"], match.ID=FALSE)
+#The result is transformed to match those above
+world_centroids <- spTransform(world_centroids, CRS("+init=epsg:4326"))
+#The result is then outputted as a shapefile, in the same way that the others are
+writeOGR(world_centroids, "shapefiles/World_centroids.shp", layer ="centroids", driver="ESRI Shapefile")
 
+#There are several countries that are missing or not specified and so these are extracted seperately 
+#These follow the same process as before
 HK <- readOGR("shapefiles/gadm36_HKG_0.shp")
 plot(HK)
 HK_centroid <- SpatialPointsDataFrame(gCentroid(HK, byid=TRUE),HK@data["GID_0"], match.ID=FALSE)
@@ -283,65 +342,5 @@ IOM_centroid <- spTransform(IOM_centroid, CRS("+init=epsg:4326"))
 writeOGR(IOM_centroid, "shapefiles/IOM_centroids.shp", layer ="centroids", driver="ESRI Shapefile")
 
 
-countries <- read_csv("country_codes.csv")
-#countries = countries[c(3,7)]
-
-world <- merge(world,
-               countries,
-               by.x = "CNTRY_NAME",
-               by.y = "Name")
-
-world_centroids <- SpatialPointsDataFrame(gCentroid(world, byid=TRUE),world@data["Code"], match.ID=FALSE)
-
-plot(world_centroids)
-world_centroids <- spTransform(world_centroids, CRS("+init=epsg:4326"))
 
 
-writeOGR(world_centroids, "shapefiles/World_centroids.shp", layer ="centroids", driver="ESRI Shapefile")
-
-#checking distance between points
-EPO_treatment_orig <- readOGR("shapefiles/EPO_treatment_cited.shp")
-#EPO_treatment_orig <- st_point(EPO_treatment_orig)
-EPO_treatment_orig <- st_as_sf(EPO_treatment_orig)
-EPO_treatment_dest <- readOGR("shapefiles/EPO_treatment_citing.shp")
-#EPO_treatment_dest <- st_point(EPO_treatment_dest)
-EPO_treatment_dest <- st_as_sf(EPO_treatment_dest)
-
-#EPO_treatment <- cbind(EPO_treatment_dest)
-
-#EPO_treatment_orig <- st_distance(EPO_treatment_orig, EPO_treatment_dest, by_element = TRUE)
-
-#EPO_treatment_orig <- mutate(distance=st_distance(EPO_treatment_orig, EPO_treatment_dest, by_element = T))
-
-EPO_treatment_orig$distance <- st_distance(EPO_treatment_orig$geometry, EPO_treatment_dest$geometry, by_element=T)
-
-#origin_df <- SpatialPointsDataFrame(coords = origin, data = EPO_treatment, proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
- 
-#EPO_treatment <- EPO_treatment %>% mutate(distance = st_distance(geometry_x, geometry_y, by_element = T))
-
-
-
-
-UK_patents <- read_csv("Patents data/all_UK_patents_region.csv")
-UK_cited <- read_csv("Patents data/cited_regions_count.csv")
-
-UK <- merge(UK_2010,
-            UK_patents,
-            by.x = "NUTS_ID", 
-            by.y = "reg_code")
-UK  <- merge(UK,
-             UK_cited,
-             by.x = "NUTS_ID",
-             by.y = "reg_code")
-
-tmap_mode("view")
-
-tm_shape(UK)+
-   tm_fill("inv_share.x")+
-   tm_borders(lwd = 1)
-
-tm_shape(UK)+
-   tm_fill("inv_share.y")+
-   tm_borders(lwd =1)
-
-citing <- read_csv("Patents data/citing_regions_count.csv")
